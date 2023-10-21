8bc5c1ed4892e80c6d2e16d0a41a7631c9eb3d41
Chaimae Chakir <96090271+Chaimae-Chakir@users.noreply.github.com>
2023-10-21 01:44:32 +0100

    First commit

---
+++ DP_Composite/src/comp/Test.java
@@ -0,0 +1,22 @@
+package comp;
+
+public class Test {
+    public static void main(String[] args) {
+        Folder root=new Folder("Design Patterns");
+        Folder d1=(Folder) root.addComponent(new Folder("Comportement"));
+        Folder d2=(Folder) root.addComponent(new Folder("Structure"));
+        Folder d3=(Folder) root.addComponent(new Folder("Création"));
+        d1.addComponent(new File("Strategy"));
+        d1.addComponent(new File("Observer"));
+        d2.addComponent(new File("Decorateur"));
+        d2.addComponent(new File("Composie"));
+        d2.addComponent(new File("Adapter"));
+        d2.addComponent(new File("Singleton"));
+        d3.addComponent(new File("Builder"));
+        Folder d21= (Folder) d2.addComponent(new Folder("Str21"));
+        d21.addComponent(new File("C211"));
+        d21.addComponent(new File("C212"));
+        d21.addComponent(new File("C213"));
+        root.show();
+    }
+}
